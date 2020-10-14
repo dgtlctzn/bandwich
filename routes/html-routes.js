860e48e01942db21e3ad51292412eeec4119652a
kreid333 <kai.j.reid@gmail.com>
2020-10-14 15:02:51 -0400

    created index.handlebars and view route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,6 +1,11 @@
 module.exports = function (app) {
+    app.get("/", (req, res)=> {
+        res.render("index");
+    });
 
     app.get("/workstation", (req, res)=> {
         res.render("workstation");
     });
+
+
 }
