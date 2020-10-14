91a1cd2612ce51b496fe90ece8acdc9d2c37a45b
Joseph Perry <josephperry720@gmail.com>
2020-10-14 11:04:43 -0400

    added basic post route

---
+++ routes/api-routes.js
@@ -0,0 +1,28 @@
+const db = require("../models");
+
+module.exports = function(app) {
+
+    app.post("/api/audio", (req, res) => {
+        db.audioFiles.create({
+            audioFile: req.body.audio,
+            path: req.body.path,
+        }).then(()=> {
+
+        }).catch(err => {
+
+        })
+    });
+
+
+    //     connection.query(
+    //       "INSERT INTO blobber (download, file) VALUES (?, ?)",
+    //       [req.body.download, req.body.data],
+    //       (err, data) => {
+    //         if (err) {
+    //           res.sendStatus(500).end();
+    //         }
+    //         console.log('here')
+    //       }
+    //     );
+    //   });
+}
