23a5acad7a258e1effb16ca654b95e612c0c7307
Patrick Hannan <patrick@hannan.com>
2020-10-14 10:54:30 -0400

    Updated the association between projects and audiofiles because it got deleted.

--- models/audiofiles.js
+++ models/audiofiles.js
@@ -4,5 +4,15 @@ module.exports = function(sequelize, DataTypes) {
       path: DataTypes.STRING,
       projectId: DataTypes.INTEGER
     });
+
+    audioFiles.associate = function(models) {
+      // We're saying that an audioFiles should belong to a Project
+      // audioFiles can't be created without an Project due to the foreign key constraint
+      audioFiles.belongsTo(models.Project, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+    };
     return audioFiles;
   };
