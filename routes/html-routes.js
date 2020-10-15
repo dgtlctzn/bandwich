0a064020952a0171df8c811db333ef0a7d81f4f4
Joseph Perry <josephperry720@gmail.com>
2020-10-15 15:08:34 -0400

    routed audio files to href tags on page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,45 +6,32 @@ module.exports = function (app) {
   app.get("/", (req, res) => {
     res.render("index");
   });
-  
+
   app.get("/projects", (req, res) => {
-    db.Project.findAll().then(function(result){
-      console.log(result)
+    db.Project.findAll().then(function (result) {
+      console.log(result);
       res.render("songs-dir", { project: result });
-    })
+    });
   });
 
   app.get("/workstation/:id", (req, res) => {
-    console.log("this is req.params.id: " + req.params.id)
+    console.log("this is req.params.id: " + req.params.id);
     db.Project.findOne({
       where: {
         id: req.params.id,
       },
-      include: db.Audiofile
+      include: db.Audiofile,
     }).then((project) => {
-      // and then displays it via handlebars
-      // console.log('here')
-      // console.log(project);
-      if (project.Audiofiles.length > 0) {
-        for (let file of project.Audiofiles) {
-          // console.log(file.path)
-          // item.download = "audio/" + item.download;
-          wav = new WaveFile();
-          const wavData = file.audiotext;
-          fs.writeFileSync(
-            "./public/audio/" + file.path,
-            Buffer.from(wavData.replace("data:audio/wav;base64,", ""), "base64")
-          );
-          file.path = "audio/" + file.path;
-        }
-        // console.log(project);
-        // console.log(project.Audiofiles[0].path);
-        res.render("workstation", { project: project });
-      } else {
-        res.render("workstation");
+      for (let file of project.Audiofiles) {
+        wav = new WaveFile();
+        const wavData = file.audiotext;
+        fs.writeFileSync(
+          "./public/audio/" + file.path,
+          Buffer.from(wavData.replace("data:audio/wav;base64,", ""), "base64")
+        );
+        file.path = "audio/" + file.path;
       }
-
-      // res.render("workstation", { project: project });
+      res.render("workstation", { project: project });
     });
   });
 };
