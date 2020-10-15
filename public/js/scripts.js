c1505ea339dea254a8e7a030ba5af2d059ae78b4
Andrew Stewart <1andrewstewart@gmail.com>
2020-10-15 13:14:59 -0400

    input populates with temp title

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -128,14 +128,27 @@ $(document).ready(function () {
       location.assign("/workstation/" + project.id);
     });
   });
+
+  saveTrackEl.on("click", function() {
+    console.log(this.children[0].innerHTML);
+    var currentTitle = this.children[0].innerHTML;
+    $("#proj-name").hide();
+    $("#new-name").show();
+    $("#new-name").val(currentTitle);
+    // $(this).children("input.edit").val(currentTitle);
+    // $(this).children("input.edit").show();
+    // $(this).children("input.edit").focus();
+
+  
+  })
   // when a user hits the save button it captures the text in the form and posts the song name
   // the post route in api-routes.js creates a new project in the database
   saveTrackEl.on("submit", function(event) {
     event.preventDefault()
 
     const projectName = {
-      name: $("#lname").val(),
-      id: $("#proj-num").data("id")
+      name: $("#new-name").val(),
+      id: $("#proj-name").data("id")
     }
 
     $.ajax("/api/project", {
